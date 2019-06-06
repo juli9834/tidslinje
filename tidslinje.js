d3dd7dae3c0fa14e0aa5a9a31bcca3dc82bba651
juli9834 <juli9834@stud.kea.dk>
2019-06-06 18:04:38 +0200

    lots of tests

--- tidslinje.js
+++ tidslinje.js
@@ -8,10 +8,34 @@ function loadSVG() {
         .then(svgData => {
             console.log("SVG loaded");
 
+
             // TODO: put the SVG into the DOM
             document.querySelector("#thesvg").insertAdjacentHTML("afterbegin", svgData)
 
             //create our snap objekt
             const snap = Snap("#thesvg svg");
-        })
+        });
 }
+
+
+
+async function hentJson() {
+    let myJson = await fetch("horcrux.json");
+    modal = await myJson.json();
+    visModal();
+}
+
+
+
+
+
+//TODO: hover over punkter
+//gøre punkter klikbare
+//modal vindue til alle punkter
+
+//modal.querySelector(".modal-navn").textContent = horgrux.navn;
+//modal.querySelector(".modal-created").textContent = horcrux.created;
+//modal.querySelector(":modal-murder").textContent = horcrux.murder;
+//modal.querySelector(".modal-how").textContent = horcrux.how;
+//modal.querySelector(".modal-where").textContent = horcrux.where;
+//modal.querySelector(".modal-hiding").textContent = horcrux.hiding;
